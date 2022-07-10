e23c1e0772ac898342c1ca19cbfb188bca2978dd
raj shringi <raj24shringi@gmail.com>
2022-07-10 13:10:42 +0530

    complete all todos

--- block-BNaadn/bookstore/models/Book.js
+++ block-BNaadn/bookstore/models/Book.js
@@ -4,11 +4,11 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   summary: String,
-  author: String,
   pages: Number,
   publication: String,
   cover_image: String,
   category: [String],
+  author_id: { type: Schema.Types.ObjectId, ref: "Author" },
 });
 
 module.exports = mongoose.model("Book", bookSchema);
